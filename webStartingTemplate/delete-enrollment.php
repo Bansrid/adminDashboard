fd65f1742b693ec48b5ef9e51f3bad9f870231e0
Bansrid <AstridBanks807@gmail.com>
2022-07-14 13:39:40 +0300

    FEAT add commit

---
+++ webStartingTemplate/delete-enrollment.php
@@ -0,0 +1,10 @@
+<?php
+require_once('logics/dbconnection.php');
+$sqldeletestudent=mysqli_query($conn,"DELETE FROM enrollment WHERE no="'.$_GET['id'].'"");
+if($sqldeletestudent)
+{
+    echo "user records deleted";
+    header('location:students.php');
+}
+else
+?>
