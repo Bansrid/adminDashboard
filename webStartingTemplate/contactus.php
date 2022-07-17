fa08cff9696433ff42694fb749acd1b63f19d2b3
Bansrid <AstridBanks807@gmail.com>
2022-07-17 15:32:04 +0300

    FEAT plus commit

--- webStartingTemplate/contactus.php
+++ webStartingTemplate/contactus.php
@@ -1,38 +1,60 @@
-<?php require_once('includes/sidebar.php') ?>
- <!-- contact us page -->
- <div class="row pt-5">
-                <h1>Contact us</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, ex amet sit accusamus corrupti dignissimos magni iste laudantium quae esse perspiciatis tempora totam accusantium impedit omnis illo consequuntur? Tempora, animi. Non quod aspernatur, qui cumque quo officiis error dolore sit reprehenderit</p>
-                <form action= "index.php" method= "POST">
-                 <div class="row">
-                <div class="mb-3 col-lg-6">
-                    <label for="firstName" class="form-label">First Name</label>
-                    <input type="text" name="firstname" class="form-control" placeholder="enter your first name">
-                </div >
-                <div class="mb-3 col-lg-6">
-                    <label for="lastName" class="form-label">Last Name</label>
-                    <input type="text" name="lastname" class="form-control" placeholder="enter your last name">
-                </div>
-                </div>
-                <div class="row">
-                    <div class="mb-3 col-lg-6">
-                        <label for="emailAddress" class="form-label">Email Address</label>
-                        <input type="text" name="email" class="form-control" placeholder="enter your email address">
-                    </div >
-                    <div class="mb-3 col-lg-6">
-                        <label for="Phonenumber" class="form-label">Phone number</label>
-                        <input type="text" name="phonenumber" class="form-control" placeholder="enter your phone number">
-                    </div>
-                    </div>
-                    <div class="row">
-                       <div class="mb-3 col-lg-12">
-                           <label for="Message"class="form label">your Message</label>
-                           <textarea cols="30" name="message" rows="5" class="form-control"></textarea>
-                       </div>
-                    </div>
-                    <br>
-                    <button type="submit" name= "submitButton" class="btn btn-primary">Send a Message</button>
-            </form>
-        </div>
-        <!-- End contact us page -->  
-    </div>
+<?php
+require_once('logics/dbconnection.php');
+$sqlquery = mysqli_query($conn,"SELECT * FROM contactus");
+?>
+<!DOCTYPE html>
+<html>
+<?php require_once('includes/headers.php')?>
+<body>
+	<!-- All our code. write here   -->
+	<div class="header">
+	<?php require_once('includes/navbar.php')?>
+	</div>
+	<div class="sidebar">
+	<?php require_once('includes/sidebar.php')?>
+	</div>
+	<div class="main-content">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-12 pt-3">
+					<div class="card-header bg-dark text-white text-center">
+						<span>Contactus</span>
+					</div>
+					<div class="card-body">
+						<table class="table table-striped table-hover">
+							<thead>
+								<tr>
+									<th>No.</th>
+									<th>Fullname Name</th>
+									<th>Phone Number</th>
+									<th>Email</th>
+									<th>message</th>
+									<th>created_at</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php while($fetchRecords = mysqli_fetch_array($sqlquery)) {?>
+									<tr>
+										<td><?php echo $fetchRecords['no']?></td>
+										<td><?php echo $fetchRecords['fullnamename']?></td>
+										<td><?php echo $fetchRecords['phonenumber']?></td>
+										<td><?php echo $fetchRecords['email']?></td>
+										<td><?php echo $fetchRecords['message']?></td>
+										<td><?php echo $fetchRecords['created_at']?></td>
+									</tr>
+								<?php }?>
+								
+							</tbody>
+						</table>
+					</div>
+					<div class="cardfooter"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+   
+	
+	<?php require_once('includes/scripts.php')?>
+</body>
+</html>
+              
